93ebf50b97d42470f134ac7bad880bb742dc9798
anewgent <anewgent@geontech.com>
2018-07-09 15:53:45 -0400

    Removed 'range' and 'subtype' from possible optional keys in fins.json

--- codegen/templates/ip_params.vhd
+++ codegen/templates/ip_params.vhd
@@ -23,11 +23,11 @@ package {{ fins['name'] }}_params is
 
 -- Parameters
 {% for param in fins['params'] -%}
-{%- if 'range' in param and 'sub_type' in param %}
-type {{ param['hdl_type'] }} is array ({{ param['range'][0] }} to {{ param['range'][1] }}) of {{ param['sub_type'] }};
+{%- if param['value'] is iterable and param['value'] is not string and not 'hdl_type' in param %}
+type t_{{ param['name'] }} is array (0 to {{ param['value']|length-1 }}) of integer;
 {% endif -%}
 constant {{ param['name'] }} :
-{%- if param['value'] is iterable and param['value'] is not string %} {{ param['hdl_type'] }} := ({{ param['value']|join(', ') }});
+{%- if param['value'] is iterable and param['value'] is not string %} t_{{ param['name'] }} := ({{ param['value']|join(', ') }});
 {% elif 'hdl_type' in param %} {{ param['hdl_type'] }} := {{ param['value'] }};
 {% elif param['value'] is string %} string := "{{ param['value'] }}";
 {% elif param['value'] is sameas true or param['value'] is sameas false %} boolean := {{ param['value']|lower }};
