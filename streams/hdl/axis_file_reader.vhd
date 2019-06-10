12afee77f0f3777e6ca683c1bcb3072157544904
David Banks <dbanks@geontech.local>
2019-06-10 09:44:39 -0400

    Backpressure and 'enable' in axis_file_reader. 64-bit max axilite width

--- streams/hdl/axis_file_reader.vhd
+++ streams/hdl/axis_file_reader.vhd
@@ -28,6 +28,7 @@ entity axis_file_reader is
   port (
     simulation_done : in  boolean;
     clk             : in  std_logic;
+    enable          : in  std_logic;
     m_axis_tready   : in  std_logic;
     m_axis_tvalid   : out std_logic;
     m_axis_tlast    : out std_logic;
@@ -42,67 +43,96 @@ begin
 
   w_read_file : process
     variable file_status    : file_open_status := NAME_ERROR;
+    variable file_done      : boolean := false;
     file     read_file      : text;
     variable read_line      : line;
     variable read_value     : std_logic_vector(G_DATA_WIDTH-1 downto 0);
     variable sample_counter : integer := 0;
     variable period_counter : integer := 0;
-    variable axis_tvalid    : std_logic;
-    variable axis_tlast     : std_logic;
-    variable axis_tdata     : std_logic_vector(G_DATA_WIDTH-1 downto 0);
+    variable axis_tvalid    : std_logic := '0';
+    variable axis_tlast     : std_logic := '0';
+    variable axis_tdata     : std_logic_vector(G_DATA_WIDTH-1 downto 0) := (others => '0');
   begin
 
     -- Use "simulation_done" to suspend operations
     if (NOT simulation_done) then
-
-      -- Read at the rising edge of the clock
+      -- Start to read at the rising edge of the clock
       wait until rising_edge(clk);
 
-      --******************************************
-      -- Calculate the values for the variables
-      --******************************************
-      -- Set defaults
-      axis_tvalid := '0';
-      axis_tlast  := '0';
-      axis_tdata  := (others => '0');
+      if (enable = '0') then
+        -- When disabled, set output signals low
+        m_axis_tvalid <= '0';
+        m_axis_tlast  <= '0';
+        m_axis_tdata  <= (others => '0');
 
-      -- When we get a TREADY, read the next value
-      if (m_axis_tready = '1') then
-        -- Respect the sample period
-        if ((period_counter rem G_SAMPLE_PERIOD) = 0) then
-          -- Open the file if it's not open already
-          if (file_status /= OPEN_OK) then
-            file_open(file_status, read_file, G_FILE_PATH, READ_MODE);
+      else
+        --******************************************
+        -- Calculate the values for the variables
+        --******************************************
+
+        if (file_done) then
+          -- When the file is done and a transaction occurs, reset signals to 0
+          -- (This is the last AXIS for the file)
+          if (axis_tvalid = '1' AND m_axis_tready = '1') then
+            axis_tvalid := '0';
+            axis_tlast  := '0';
+            axis_tdata  := (others => '0');
           end if;
-          -- Grab a valid value from the file if we haven't reached the EOF
-          if (NOT endfile(read_file)) then
-            -- Read a hexadecimal string from a line of the file
-            readline(read_file, read_line);
-            hread(read_line, read_value);
-            -- Set the output values
-            axis_tvalid := '1';
-            axis_tdata  := read_value;
-            -- TLAST is active only when we've reached the final
-            -- value in an AXI-Stream packet
-            if (sample_counter = G_PACKET_SIZE-1) then
-              axis_tlast := '1';
-              sample_counter := 0;
-            else
-              axis_tlast := '0';
-              sample_counter := sample_counter + 1;
+          -- Note: the file_done flag might NOT mean that the last AXIS transaction has occurred.
+          -- This is the case if the file is done being read but the last chunk of data has not yet
+          -- been part of a valid AXIS transaction. So, when the file is done AND a transaction
+          -- occurs during that cycle or a later cycle, the data is done being consumed and AXIS
+          -- signals can be reset to 0.
+        else
+
+          -- When we get a TREADY, read the next value
+          if (m_axis_tready = '1') then
+            -- Respect the sample period
+            if ((period_counter rem G_SAMPLE_PERIOD) = 0) then
+              -- Open the file if it's not open already
+              if (file_status /= OPEN_OK) then
+                file_open(file_status, read_file, G_FILE_PATH, READ_MODE);
+              end if;
+              -- Grab a valid value from the file if we haven't reached the EOF
+              if (NOT endfile(read_file)) then
+                -- Read a hexadecimal string from a line of the file
+                readline(read_file, read_line);
+                hread(read_line, read_value);
+                -- Set the output values
+                axis_tvalid := '1';
+                axis_tdata  := read_value;
+                -- TLAST is active only when we've reached the final
+                -- value in an AXI-Stream packet
+                if (sample_counter = G_PACKET_SIZE-1) then
+                  axis_tlast := '1';
+                  sample_counter := 0;
+                else
+                  axis_tlast := '0';
+                  sample_counter := sample_counter + 1;
+                end if;
+              else
+                -- If the last AXIS transaction for the file occurs on this same cycle
+                -- (which happens to be when the file is done being read) reset signals to 0
+                if (axis_tvalid = '1') then
+                  axis_tvalid := '0';
+                  axis_tlast := '0';
+                  axis_tdata  := (others => '0');
+                end if;
+                -- Set the file_done flag for reference during next cycle
+                file_done := true;
+              end if;
             end if;
           end if;
+          -- Increment the period counter
+          period_counter := period_counter + 1;
         end if;
-        -- Increment the period counter
-        period_counter := period_counter + 1;
+        --******************************************
+        -- Set outputs to the variables
+        --******************************************
+        m_axis_tvalid <= axis_tvalid;
+        m_axis_tlast  <= axis_tlast;
+        m_axis_tdata  <= axis_tdata;
       end if;
-
-      --******************************************
-      -- Set outputs to the variables
-      --******************************************
-      m_axis_tvalid <= axis_tvalid;
-      m_axis_tlast  <= axis_tlast;
-      m_axis_tdata  <= axis_tdata;
 
     else
 
